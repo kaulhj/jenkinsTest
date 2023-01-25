9fea26bea248c885a50d91e5fc8789432d7cc623
Jinger <lhj69382@naver.com>
2023-01-25 21:30:28 +0900

    first commit11

--- src/main/java/com/example/kenu/WebController.java
+++ src/main/java/com/example/kenu/WebController.java
@@ -10,6 +10,6 @@ public class WebController {
     @GetMapping("/")
     @ResponseBody
     public String main() {
-        return "<a href=\"https://infoq.com2\">infoq.com1000</a>";
+        return "<a href=\"https://infoq.com2\">infoq.com1004</a>";
     }
 }
