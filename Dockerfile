905ac03b90b553cd0e5bcc81e617107699a2bde3
Jinger <lhj69382@naver.com>
2023-01-25 21:47:52 +0900

    first commit14

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM openjdk:11-jre-slim-buster
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
-EXPOSE 8062
+EXPOSE 8075
--- src/main/java/com/example/kenu/WebController.java
+++ src/main/java/com/example/kenu/WebController.java
@@ -1,5 +1,6 @@
 package com.example.kenu;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
+
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/")
     @ResponseBody
     public String main() {
-        return "<a href=\"https://infoq.com2\">infoq.com1004</a>";
+        return "<a href=\"https://infoq.com2\">infoq.com1004, port : </a>"+port;
     }
 }
